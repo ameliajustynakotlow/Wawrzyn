a4fb8bbc3436f79c80c459c2db6665849bf6a321
JustynaAmeliaKotlow <58250094+JustynaAmeliaKotlow@users.noreply.github.com>
2022-07-10 22:43:26 +0200

    Added a full width mobile menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+var isMenuOpen = false;
+
 $(document).ready(function() {
   $(window).on('scroll', function() {
     var scrollPosition = $(window).scrollTop();
@@ -7,5 +9,32 @@ $(document).ready(function() {
     } else {
       $('#main-nav').removeClass('bg-primary').addClass('bg-none');
     }
-  })
+  });
+
+  $('#toggle-menu').on('click', function() {
+  	toggleMobileMenu()
+  });
+
+  $('.mobile-link').on('click', function() {
+  	closeMobileMenu();
+  	isMenuOpen = false;
+  });
 });
+
+function toggleMobileMenu() {
+	if (isMenuOpen) {
+		closeMobileMenu();
+	} else {
+		openMobileMenu();
+	}
+
+	isMenuOpen = !isMenuOpen;	
+}
+
+function openMobileMenu() {
+  $('#mobile-nav').css('width', '100%');
+}
+
+function closeMobileMenu() {
+  $('#mobile-nav').css('width', '0');
+}
